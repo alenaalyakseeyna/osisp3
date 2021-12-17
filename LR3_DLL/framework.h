c385787b919d8d4426aaae4a04a732e3802786ae
alenaalyakseeyna <87442182+alenaalyakseeyna@users.noreply.github.com>
2021-12-17 21:36:12 +0300

    Add files via upload

---
+++ LR3_DLL/framework.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
+// Файлы заголовков Windows
+#include <windows.h>
+#include <iostream>
+#include <stdlib.h>
+#include <iostream>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <vector>
+#include <string>
+#include <fstream>
